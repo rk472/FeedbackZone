18a07dfc34b2ea42fcdd687b4648eb6213d1afa3
Ramakanta <33664106+rk472@users.noreply.github.com>
2018-07-02 00:00:59 +0530

    Initial Commit

--- app/src/main/java/com/smarttersstudio/feedbackzone/HomeActivity.java
+++ app/src/main/java/com/smarttersstudio/feedbackzone/HomeActivity.java
@@ -65,20 +65,13 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds it
-        //   ems to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_logout) {
             mAuth.signOut();
             startActivity(new Intent(HomeActivity.this,LoginActivity.class));
