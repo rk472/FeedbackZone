3e500ac3c2675cf0f9f42614e3337c03e0f6f1f1
Ramakanta <33664106+rk472@users.noreply.github.com>
2018-07-01 23:59:48 +0530

    Initial Commit

--- app/src/main/java/com/smarttersstudio/feedbackzone/HomeActivity.java
+++ app/src/main/java/com/smarttersstudio/feedbackzone/HomeActivity.java
@@ -1,51 +1,30 @@
 package com.smarttersstudio.feedbackzone;
 
-import android.graphics.Color;
-import android.graphics.Typeface;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.FrameLayout;
-import android.widget.Toast;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import com.github.mikephil.charting.animation.Easing;
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.formatter.PercentFormatter;
-import com.github.mikephil.charting.highlight.Highlight;
-import com.github.mikephil.charting.interfaces.datasets.IDataSet;
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
-import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.firebase.auth.FirebaseAuth;
 import com.smarttersstudio.feedbackzone.Adapters.SwipeAdapter;
 import com.smarttersstudio.feedbackzone.Fragmets.AdministrationBottom;
-import com.smarttersstudio.feedbackzone.Fragmets.AdministrationTop;
 import com.smarttersstudio.feedbackzone.Fragmets.FoodBottom;
-import com.smarttersstudio.feedbackzone.Fragmets.FoodTop;
 import com.smarttersstudio.feedbackzone.Fragmets.ManagerBottom;
-import com.smarttersstudio.feedbackzone.Fragmets.ManagerTop;
 import com.smarttersstudio.feedbackzone.Fragmets.SecurityBottom;
-import com.smarttersstudio.feedbackzone.Fragmets.SecurityTop;
-
-import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
-    private PieChart mChart;
-    private Typeface tf;
     private FragmentTransaction fragmentTransaction;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        initializeChart();
+        mAuth = FirebaseAuth.getInstance();
+
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setOffscreenPageLimit(1);
         SwipeAdapter swipeAdapter = new SwipeAdapter(getSupportFragmentManager());
@@ -83,80 +62,30 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
     }
-    private void initializeChart(){
-        //Chart Initialization
-        mChart = (PieChart)findViewById(R.id.chart1);
-        mChart.setUsePercentValues(true);
-        mChart.getDescription().setEnabled(false);
-        mChart.setExtraOffsets(5, 10, 5, 5);
-        mChart.setDragDecelerationFrictionCoef(0.95f);
-        tf = Typeface.createFromAsset(getAssets(), "OpenSans-Regular.ttf");
-        mChart.setCenterTextTypeface(Typeface.createFromAsset(getAssets(), "OpenSans-Light.ttf"));
-        mChart.setCenterText("Summery");
-        mChart.setExtraOffsets(20.f, 0.f, 20.f, 0.f);
-        mChart.setDrawHoleEnabled(true);
-        mChart.setHoleColor(Color.WHITE);
-        mChart.setTransparentCircleColor(Color.WHITE);
-        mChart.setTransparentCircleAlpha(110);
-        mChart.setHoleRadius(58f);
-        mChart.setTransparentCircleRadius(61f);
-        mChart.setDrawCenterText(true);
-        mChart.setRotationAngle(0);
-        mChart.setRotationEnabled(true);
-        mChart.setHighlightPerTapEnabled(true);
-        setData(4, 100);
-        //mChart.animateY(1400, Easing.EaseInOutQuad);
-        Legend l = mChart.getLegend();
-        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
-        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
-        l.setOrientation(Legend.LegendOrientation.VERTICAL);
-        l.setDrawInside(false);
-        l.setEnabled(false);
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds it
+        //   ems to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
-    private void setData(int count, float range) {
 
-        float mult = range;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-        ArrayList<PieEntry> entries = new ArrayList<PieEntry>();
-
-        for (int i = 0; i < count; i++) {
-            String mParties = null;
-            entries.add(new PieEntry((float) (Math.random() * mult) + mult / 5, "Label"+i+""));
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_logout) {
+            mAuth.signOut();
+            startActivity(new Intent(HomeActivity.this,LoginActivity.class));
+            finishAffinity();
+            finish();
         }
 
-        PieDataSet dataSet = new PieDataSet(entries, "Election Results");
-        dataSet.setSliceSpace(3f);
-        dataSet.setSelectionShift(5f);
-        // add a lot of colors
-        ArrayList<Integer> colors = new ArrayList<Integer>();
-        for (int c : ColorTemplate.VORDIPLOM_COLORS)
-            colors.add(c);
-        for (int c : ColorTemplate.JOYFUL_COLORS)
-            colors.add(c);
-        for (int c : ColorTemplate.COLORFUL_COLORS)
-            colors.add(c);
-        for (int c : ColorTemplate.LIBERTY_COLORS)
-            colors.add(c);
-        for (int c : ColorTemplate.PASTEL_COLORS)
-            colors.add(c);
-        colors.add(ColorTemplate.getHoloBlue());
-        dataSet.setColors(colors);
-        dataSet.setValueLinePart1OffsetPercentage(80.f);
-        dataSet.setValueLinePart1Length(0.2f);
-        dataSet.setValueLinePart2Length(0.4f);
-        //dataSet.setUsingSliceColorAsValueLineColor(true);
-
-        //dataSet.setXValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
-        dataSet.setYValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
-
-        PieData data = new PieData(dataSet);
-        data.setValueFormatter(new PercentFormatter());
-        data.setValueTextSize(11f);
-        data.setValueTextColor(Color.BLACK);
-        data.setValueTypeface(tf);
-        mChart.setData(data);
-        // undo all highlights
-        mChart.highlightValues(null);
-        mChart.invalidate();
+        return super.onOptionsItemSelected(item);
     }
 }
