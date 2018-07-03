3822933fe56f121e00900bad4f811e25834ec1e2
rk472 <33664106+rk472@users.noreply.github.com>
2018-07-03 17:30:29 +0530

    food feedback done

--- app/src/main/java/com/smarttersstudio/feedbackzone/Fragmets/FoodBottom.java
+++ app/src/main/java/com/smarttersstudio/feedbackzone/Fragmets/FoodBottom.java
@@ -1,16 +1,35 @@
 package com.smarttersstudio.feedbackzone.Fragmets;
 
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.smarttersstudio.feedbackzone.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FoodBottom extends Fragment {
@@ -20,15 +39,78 @@ public class FoodBottom extends Fragment {
     private Button submitButton;
     private CheckBox anonymous;
     private EditText feedBackText;
+    private FirebaseAuth mAuth;
+    private DatabaseReference feedbackRef;
+    private ProgressDialog progressDialog;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         root=inflater.inflate(R.layout.fragment_food_bottom, container, false);
-
+        submitButton=root.findViewById(R.id.food_submit);
+        feedBackText=root.findViewById(R.id.food_feedback);
+        feedbackRef= FirebaseDatabase.getInstance().getReference().child("feedback").child("food");
+        mAuth=FirebaseAuth.getInstance();
+        progressDialog=new ProgressDialog(getActivity());
+        progressDialog.setMessage("Please wait while we are logging you in..");
+        progressDialog.setTitle("Please Wait");
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.setCancelable(false);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String feedback=feedBackText.getText().toString().trim();
+                if(TextUtils.isEmpty(feedback)){
+                    Toast.makeText(getActivity(), "can't give empty feedback..", Toast.LENGTH_SHORT).show();
+                }else {
+                    progressDialog.show();
+                    new AlertDialog.Builder(getActivity())
+                            .setTitle("Give Feedback")
+                            .setMessage("Do You want to show your name ?")
+                            .setPositiveButton("Yes",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialogInterface, int i) {
+                                            String uid=mAuth.getCurrentUser().getUid();
+                                            giveFeedBack(feedback,uid);
+                                        }
+                                    }).setNegativeButton("No",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    giveFeedBack(feedback,"1");
+                                }
+                            }).show();
+                }
+            }
+        });
 
         return root;
+    }
+    void giveFeedBack(String feedback,String uid){
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+        String d=formatter.format(date);
+        Map m=new HashMap();
+        m.put("uid",uid);
+        m.put("feedback",feedback);
+        m.put("date",d);
+        feedbackRef.push().updateChildren(m).addOnSuccessListener(new OnSuccessListener() {
+            @Override
+            public void onSuccess(Object o) {
+                Toast.makeText(getActivity(), "FeedBack successfully posted...", Toast.LENGTH_SHORT).show();
+                feedBackText.setText("");
+                progressDialog.hide();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                progressDialog.hide();
+            }
+        });
+
     }
 
 }
