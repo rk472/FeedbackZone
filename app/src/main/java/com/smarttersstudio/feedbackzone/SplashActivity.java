87ef8f645e3897d0bb8972474d23bcb3e20b724c
Ramakanta <33664106+rk472@users.noreply.github.com>
2018-07-06 17:58:48 +0530

    Initial Commit

--- app/src/main/java/com/smarttersstudio/feedbackzone/SplashActivity.java
+++ app/src/main/java/com/smarttersstudio/feedbackzone/SplashActivity.java
@@ -4,14 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 public class SplashActivity extends AppCompatActivity {
-
+    private Button b1,b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        b1 = findViewById(R.id.goFeedback);
+        b2 = findViewById(R.id.goForum);
+        Animation animationFadeIn = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        b1.startAnimation(animationFadeIn);
+        b2.startAnimation(animationFadeIn);
     }
 
     public void goToFeedBack(View view) {
